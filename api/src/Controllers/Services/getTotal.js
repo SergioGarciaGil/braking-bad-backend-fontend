1f3e40de9b877afd1a97efe45704bb548815d4bc
SergioGarciaGil <sergio.cuerda@gmail.com>
2022-04-09 12:37:54 -0500

    update

--- api/src/Controllers/Services/getTotal.js
+++ api/src/Controllers/Services/getTotal.js
@@ -1,10 +1,22 @@
-const getApiInfo = require("./getApiInfo");
-const getDbInfo = require("./getDbInfo");
+// const axios = require("axios");
 
-const getTotal = async () => {
-  const apiInfo = await getApiInfo();
-  const dbInfo = await getDbInfo();
-  const infoTotal = apiInfo.concat(dbInfo);
-  return infoTotal;
-};
-module.exports = getTotal;
+// const getApiInfo = async () => {
+//   const apiUrl = await axios.get(
+//     "https://www.breakingbadapi.com/api/characters"
+//   );
+//   const apiInfo = apiUrl.data.map((el) => {
+//     //me va atraer la informacion de la api
+//     return {
+//       name: el.name,
+//       imagen: el.img,
+//       nickname: el.nickname,
+//       status: el.status,
+//       id: el.char_id,
+//       occupation: el.occupation,
+//       birthday: el.birthday,
+//       appearance: el.appearance.map((el) => el),
+//     };
+//   });
+//   return apiInfo;
+// };
+// module.exports = getApiInfo;
